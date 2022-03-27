d77d33a71258b70e29f2fbf23a273a9471740bee
Ted Shaffer <shaffer.family@gmail.com>
2022-03-27 07:36:21 -0700

    hook up code to be able to List Words.

--- src/selectors/appState.ts
+++ src/selectors/appState.ts
@@ -8,6 +8,18 @@ export const getSecondWord = (state: TedState): string => {
   return state.appState.secondWord;
 };
 
+export const getLettersAtExactLocation = (state: TedState): string[] => {
+  return state.appState.lettersAtExactLocation;
+};
+
+export const getLettersNotAtExactLocation = (state: TedState): string[] => {
+  return state.appState.lettersNotAtExactLocation;
+};
+
+export const getLettersNotInWord = (state: TedState): string => {
+  return state.appState.lettersNotInWord;
+};
+
 export const getPossibleWords = (state: TedState): string[] => {
   return state.appState.possibleWords;
 };
